b900a0439d15fc0bdb66a15acb2eeec9590d80cd
matt <mattcave814@gmail.com>
2022-06-18 13:29:34 -0500

    Fixed some bugs, cleaned up unused HTML

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Webpage with calculator that can...
 -Add, subtract, multiply, and divide
 -Support decimals
--Backspace when you make a mistake
 -Be used with keyboard
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let operatorCalled = false; //initialize operator switch
 let midEquation = false; //lets operators/calc button know if they should calculate answer if user is mid equation
-let x, y, currentOperator, baseLength; //initialize both sides of equation and current operator and baselength of number
+let x, y, currentOperator, baseLength, isExponent;
 const maxDigits = 7; //display caps at 7 characters (then rounds decimals or goes to scientific notation)
 
 const buttons = document.querySelectorAll('button');
@@ -9,14 +9,23 @@ const display = document.querySelector('.display');
 function add(x,y) {return +x + +y}; //turn strings into numbers so they don't concat
 function subtract(x,y) {return x-y};
 function multiply(x,y) {return x*y};
-function divide(x,y) {return (y==='0') ? 'Error':x/y;};
+function divide(x,y) {return (y==='0') ? 'Error':x/y;}; //can't divide by 0
 function operate(x,operator,y) {return window[operator.id](x,y)};
 
 buttons.forEach(btn => btn.addEventListener('click', e => updateDisplay(e.target)));
 
+window.addEventListener('keydown', e => {
+    let btn = document.getElementById(e.key); //gets button with matching id
+    updateDisplay(btn); //updateDisplay function requires the button's DOM element as the input
+});
+
 function calc() {
     y = display.textContent;
     x = operate(x,currentOperator,y);
+    if (x==='Error') {
+        display.textContent = x;
+        return; //end the calc if there's an error
+    }
     baseLength = Math.floor(x).toString().length; //length of main number (without decimals)
 
     x = (baseLength > maxDigits) ? x.toExponential(2):x = Number(x.toFixed(maxDigits-baseLength));
@@ -27,14 +36,20 @@ function calc() {
     midEquation = false;
 }
 
+function setOperator(btn) {
+    operatorCalled = true;
+    currentOperator = btn;
+    btn.classList.add('activated');
+}
+
 function updateDisplay(btn) {
     if (btn.classList.contains('number')) {
         if (!operatorCalled) {
-            if (display.textContent.toString().length >= maxDigits) return;
+            if (display.textContent.toString().length >= maxDigits) return; //can't type more than 7 characters
             if (display.textContent === '0') {
-                display.textContent = btn.textContent;
+                display.textContent = btn.textContent; //if display shows only a 0 then it gets replaced by next digit
             } else {
-                display.textContent += btn.textContent;
+                display.textContent += btn.textContent; //concat next digit to display if not 0
             }
         } else {
             operatorCalled = false;
@@ -47,38 +62,34 @@ function updateDisplay(btn) {
     if (btn.classList.contains('operator')) {
         if (midEquation) {
             calc();
-            operatorCalled = true;
-            currentOperator = btn;
-            btn.classList.add('activated');
+            setOperator(btn);
         } else {
             if (operatorCalled) {
                 currentOperator.classList.remove('activated');
-                currentOperator = btn;
-                btn.classList.add('activated');
+                setOperator(btn);
             } else {
-                operatorCalled = true;
-                currentOperator = btn;
-                btn.classList.add('activated');
+                setOperator(btn);
             }
         }
     }
     if (btn.classList.contains('equals')) {
-        if (midEquation) {
-            calc();
-        }
+        if (midEquation) calc();
     }
     if (btn.classList.contains('clear')) {
-        currentOperator.classList.remove('activated');
+        if (currentOperator) currentOperator.classList.remove('activated');
         x = y = currentOperator = undefined;
         operatorCalled = midEquation = false;
         display.textContent = '0';
     }
     if (btn.classList.contains('plusminus')) {
+        if (display.textContent.includes('e')) isExponent = true;
         display.textContent *= -1;
+        if (isExponent) {
+            display.textContent = Number(display.textContent).toExponential(2);
+            isExponent = false;
+        }
     }
     if (btn.classList.contains('decimal')) {
-        if (!display.textContent.includes('.')) {
-            display.textContent += '.';
-        }
+        if (!display.textContent.includes('.')) display.textContent += '.';
     }
 };
